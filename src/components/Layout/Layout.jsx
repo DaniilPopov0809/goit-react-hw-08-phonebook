e854e45b3abb2676f9d3336e5ba2b6c4c4482880
Daniil Popov <x6uhrox@gmail.com>
2023-06-22 22:37:25 +0300

    FIX: main

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,11 @@
-import { Box, Center, Container, Divider, Flex, Text, Link } from '@chakra-ui/react';
+import {
+  Box,
+  Center,
+  Divider,
+  Flex,
+  Text,
+  Link,
+} from '@chakra-ui/react';
 import AppBar from '../AppBar/AppBar';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
@@ -6,37 +13,46 @@ import { Spinner } from '@chakra-ui/react';
 
 const Layout = () => {
   return (
-    <Flex justifyContent="center" alignItems="center">
-      <Container maxW="container.lg" p={4}>
+    <Flex
+      justifyContent="center"
+      alignItems="stretch"
+      minHeight="100vh"
+      flexDirection="column"
+    >
+      <Box as="main" flex="1">
         <AppBar />
         <Divider marginBottom={5} />
-        <Box as="main">
-          <Flex justifyContent="center" alignContent="center">
-            <Center>
-              <Suspense
-                fallback={
-                  <Spinner
-                    thickness="4px"
-                    speed="0.65s"
-                    emptyColor="gray.200"
-                    color="blue"
-                    size="lg"
-                  />
-                }
-              >
-                <Outlet />
-              </Suspense>
-            </Center>
-          </Flex>
-        </Box>
-        <Divider marginTop={5} />
-        <Box as='footer' p={4}>
-          <Flex justifyContent='center' alignItems='center' flexDirection='column'>
-          <Text as='p'>©2023 Daniil Popov</Text>
-          <Link href="mailto:x6uhrox@gmail.com" _hover={{ color: 'blue' }}>x6uhrox@gmail.com</Link>
-          </Flex>
-        </Box>
-      </Container>
+        <Flex justifyContent="center" alignContent="center" flex="1">
+          <Center>
+            <Suspense
+              fallback={
+                <Spinner
+                  thickness="4px"
+                  speed="0.65s"
+                  emptyColor="gray.200"
+                  color="blue"
+                  size="lg"
+                />
+              }
+            >
+              <Outlet />
+            </Suspense>
+          </Center>
+        </Flex>
+      </Box>
+      <Divider />
+      <Box as="footer" p={4}>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+        >
+          <Text as="p">©2023 Daniil Popov</Text>
+          <Link href="mailto:x6uhrox@gmail.com" _hover={{ color: 'blue' }}>
+            x6uhrox@gmail.com
+          </Link>
+        </Flex>
+      </Box>
     </Flex>
   );
 };
